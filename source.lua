5160a124e774e0787d979cfc411ac29ea602ec26
RbxSolutions <80179841+RbxSolutions@users.noreply.github.com>
2021-03-07 00:07:00 +0100

    Initial Commit

---
+++ source.lua
@@ -0,0 +1,37 @@
+getgenv().IpProtection = true
+spoofedIP = "0.0.0.0"
+
+
+IP_Trackers = {
+    "https://v4.ident.me",
+    "https://httpbin.org/get"
+}
+
+local old
+old = hookfunction(game.HttpGet, function(self, url)
+    if type(url) == "string" and getgenv().IpProtection then
+        -- Checks if the URL is in the IP_Trackers table
+        if table.find(IP_Trackers, url) then
+            -- Checking if the protection is enabled
+            warn(url,"tried to log your IP. It was protected.")
+            return spoofedIP -- Spoofed IP
+        end
+    end
+    return old(self, url)
+end)
+
+local oldSyn
+oldSyn = hookfunction(syn.request,function(a,b)
+    if type(a) == "table" and getgenv().IpProtection then
+        for i,v in pairs(a) do
+            if i == "Url" and table.find(IP_Trackers, v) then
+                warn(v,"tried to log your IP. It was protected.")
+                return {
+                    StatusCode = 200,
+                    Body = spoofedIP
+                }
+            end
+        end
+    end
+    return oldSyn(a,b)
+end)
